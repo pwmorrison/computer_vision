d1e65fa7518aaade49c4a04991c19005e840a207
pwmorrison <pwmorrison@gmail.com>
2017-04-14 19:06:23 +1000

    Added toolbar.

--- wxpython_app.py
+++ wxpython_app.py
@@ -1,4 +1,5 @@
 import wx
+import images
 
 class EasyMenu(wx.Menu):
     _map = { wx.ID_CUT : wx.ART_CUT,
@@ -19,8 +20,8 @@ class EasyMenu(wx.Menu):
         return self.AppendItem(item)
 
 class ImagePanel(wx.Panel):
-    def __init__(self, parent):
-        super(ImagePanel, self).__init__(parent)
+    def __init__(self, parent, style):
+        super(ImagePanel, self).__init__(parent, style=style)
         # Load the image data into a Bitmap
         theBitmap = wx.Bitmap("images/house1_small_corner.jpg")
         # Create a control that can display the
@@ -48,6 +49,14 @@ class MySplitter(wx.SplitterWindow):
         # uncomment this to not allow the change
         # evt.SetSashPosition(-1)
 
+TBFLAGS = ( wx.TB_HORIZONTAL
+            | wx.NO_BORDER
+            | wx.TB_FLAT
+            #| wx.TB_TEXT
+            #| wx.TB_HORZ_LAYOUT
+            )
+
+
 class SplitterFrame(wx.Frame):
     def __init__(self, parent, title=""):
         super(SplitterFrame, self).__init__(parent, title=title)
@@ -59,16 +68,119 @@ class SplitterFrame(wx.Frame):
         # sty = wx.BORDER_SIMPLE
         sty = wx.BORDER_SUNKEN
 
-        p1 = wx.Window(splitter, style=sty)
-        p1.SetBackgroundColour("pink")
-        wx.StaticText(p1, -1, "Panel One", (5, 5))
+        if 0:
+            p1 = wx.Window(splitter, style=sty)
+            p1.SetBackgroundColour("pink")
+            wx.StaticText(p1, -1, "Panel One", (5, 5))
 
-        p2 = wx.Window(splitter, style=sty)
-        p2.SetBackgroundColour("sky blue")
-        wx.StaticText(p2, -1, "Panel Two", (5, 5))
+            p2 = wx.Window(splitter, style=sty)
+            p2.SetBackgroundColour("sky blue")
+            wx.StaticText(p2, -1, "Panel Two", (5, 5))
+        else:
+            p1 = ImagePanel(splitter, style=sty)
+            p2 = ImagePanel(splitter, style=sty)
 
         splitter.SetMinimumPaneSize(20)
         splitter.SplitVertically(p1, p2, -100)
+
+        tb = self.CreateToolBar(TBFLAGS)
+
+        # log.write("Default toolbar tool size: %s\n" % tb.GetToolBitmapSize())
+
+        self.CreateStatusBar()
+
+        tsize = (24, 24)
+        new_bmp = wx.ArtProvider.GetBitmap(wx.ART_NEW, wx.ART_TOOLBAR, tsize)
+        open_bmp = wx.ArtProvider.GetBitmap(wx.ART_FILE_OPEN, wx.ART_TOOLBAR, tsize)
+        copy_bmp = wx.ArtProvider.GetBitmap(wx.ART_COPY, wx.ART_TOOLBAR, tsize)
+        paste_bmp = wx.ArtProvider.GetBitmap(wx.ART_PASTE, wx.ART_TOOLBAR, tsize)
+
+        tb.SetToolBitmapSize(tsize)
+
+        # tb.AddSimpleTool(10, new_bmp, "New", "Long help for 'New'")
+        tb.AddLabelTool(10, "New", new_bmp, shortHelp="New", longHelp="Long help for 'New'")
+        self.Bind(wx.EVT_TOOL, self.OnToolClick, id=10)
+        self.Bind(wx.EVT_TOOL_RCLICKED, self.OnToolRClick, id=10)
+
+        # tb.AddSimpleTool(20, open_bmp, "Open", "Long help for 'Open'")
+        tb.AddLabelTool(20, "Open", open_bmp, shortHelp="Open", longHelp="Long help for 'Open'")
+        self.Bind(wx.EVT_TOOL, self.OnToolClick, id=20)
+        self.Bind(wx.EVT_TOOL_RCLICKED, self.OnToolRClick, id=20)
+
+        tb.AddSeparator()
+        tb.AddSimpleTool(30, copy_bmp, "Copy", "Long help for 'Copy'")
+        self.Bind(wx.EVT_TOOL, self.OnToolClick, id=30)
+        self.Bind(wx.EVT_TOOL_RCLICKED, self.OnToolRClick, id=30)
+
+        tb.AddSimpleTool(40, paste_bmp, "Paste", "Long help for 'Paste'")
+        self.Bind(wx.EVT_TOOL, self.OnToolClick, id=40)
+        self.Bind(wx.EVT_TOOL_RCLICKED, self.OnToolRClick, id=40)
+
+        tb.AddSeparator()
+
+        # tool = tb.AddCheckTool(50, images.Tog1.GetBitmap(), shortHelp="Toggle this")
+        tool = tb.AddCheckLabelTool(50, "Checkable", images.Tog1.GetBitmap(),
+                                    shortHelp="Toggle this")
+        self.Bind(wx.EVT_TOOL, self.OnToolClick, id=50)
+
+        self.Bind(wx.EVT_TOOL_ENTER, self.OnToolEnter)
+        self.Bind(wx.EVT_TOOL_RCLICKED, self.OnToolRClick)  # Match all
+        self.Bind(wx.EVT_TIMER, self.OnClearSB)
+
+        tb.AddSeparator()
+        cbID = wx.NewId()
+
+        tb.AddControl(
+            wx.ComboBox(
+                tb, cbID, "", choices=["", "This", "is a", "wx.ComboBox"],
+                size=(150, -1), style=wx.CB_DROPDOWN
+            ))
+        self.Bind(wx.EVT_COMBOBOX, self.OnCombo, id=cbID)
+
+        tb.AddStretchableSpace()
+        # search = TestSearchCtrl(tb, size=(150, -1), doSearch=self.DoSearch)
+        # tb.AddControl(search)
+
+        # Final thing to do for a toolbar is call the Realize() method. This
+        # causes it to render (more or less, that is).
+        tb.Realize()
+
+    def OnToolClick(self, event):
+        # self.log.WriteText("tool %s clicked\n" % event.GetId())
+        # tb = self.GetToolBar()
+        tb = event.GetEventObject()
+        tb.EnableTool(10, not tb.GetToolEnabled(10))
+
+    def OnToolRClick(self, event):
+        # self.log.WriteText("tool %s right-clicked\n" % event.GetId())
+        pass
+
+    def OnCombo(self, event):
+        # self.log.WriteText("combobox item selected: %s\n" % event.GetString())
+        pass
+
+    def OnToolEnter(self, event):
+        # self.log.WriteText('OnToolEnter: %s, %s\n' % (event.GetId(), event.GetInt()))
+
+        # if self.timer is None:
+        #     self.timer = wx.Timer(self)
+        #
+        # if self.timer.IsRunning():
+        #     self.timer.Stop()
+
+        # self.timer.Start(2000)
+        event.Skip()
+
+    def OnClearSB(self, event):  # called for the timer event handler
+        self.SetStatusText("")
+        # self.timer.Stop()
+        # self.timer = None
+
+    def OnCloseWindow(self, event):
+        # if self.timer is not None:
+        #     self.timer.Stop()
+        #     self.timer = None
+        self.Destroy()
 
 
 class MyFrame(wx.Frame):
