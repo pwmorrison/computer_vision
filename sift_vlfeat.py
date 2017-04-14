836a5eeac7e62d50e3579c6add00909d035f1ed3
Paul Morrison <pwmorrison@gmail.com>
2017-04-14 11:52:25 +1000

    Determine and apply homography, and display the transformed points and inlier flags.

--- sift_vlfeat.py
+++ sift_vlfeat.py
@@ -1,8 +1,7 @@
 from pylab import *
 from numpy import *
 from PIL import Image
-import os
-print os.environ['HOME']
+import cv2
 
 from PCV.localdescriptors import sift
 
@@ -49,41 +48,85 @@ def plot_matches(im1, im2, locs1, locs2, matchscores, show_below=True):
     axis('off')
 
 def get_matches(locs1, locs2, matchscores):
+    pts1 = []
+    pts2 = []
     for i, m in enumerate(matchscores):
         if m > 0:
-            print("%d:(%d, %d) -> %d:(%d, %d)" % (i, locs1[i][0], locs1[i][1], m, locs2[m][0], locs2[m][1]))
+            pt1 = (locs1[i][0], locs1[i][1])
+            pt2 = (locs1[m][0], locs1[m][1])
+            print("%d:(%d, %d) -> %d:(%d, %d)" % (i, pt1[0], pt1[1], m, pt2[0], pt2[1]))
+            pts1.append(pt1)
+            pts2.append(pt2)
+    return pts1, pts2
 
-# imname1 = 'C:/Users/Paul/programming-computer-vision-git/PCV/data/climbing_1_small.jpg'
-# imname2 = 'C:/Users/Paul/programming-computer-vision-git/PCV/data/climbing_2_small.jpg'
-# imname1 = 'C:/Users/Paul/programming-computer-vision-git/PCV/data/crans_1_small.jpg'
-# imname2 = 'C:/Users/Paul/programming-computer-vision-git/PCV/data/crans_2_small.jpg'
-# imname1 = 'C:/Users/Paul/programming-computer-vision-git/PCV/data/house1_small.jpg'
-# imname2 = 'C:/Users/Paul/programming-computer-vision-git/PCV/data/house2_small.jpg'
-# imname1 = 'C:/Users/Paul/programming-computer-vision-git/PCV/data/house1_small.jpg'
-# imname2 = 'C:/Users/Paul/programming-computer-vision-git/PCV/data/house1_small_corner.jpg'
-imname1 = '/home/paul/computer_vision/images/house1_small.jpg'
-imname2 = '/home/paul/computer_vision/images/house1_small_corner.jpg'
+def get_homography(pts1, pts2):
 
-# process and save features to file
-params="--edge-thresh 10 --peak-thresh 5 --verbose"
-sift.process_image(imname1, imname1+'.sift', params=params)
-sift.process_image(imname2, imname2+'.sift', params=params)
+    src_pts = np.array(pts1)
+    dst_pts = np.array(pts2)
 
-# read features and match
-l1,d1 = sift.read_features_from_file(imname1+'.sift')
-l2,d2 = sift.read_features_from_file(imname2+'.sift')
-matchscores = sift.match_twosided(d1, d2)
+    # I think M is the homography matrix, and mask indicates the inlying matches.
+    H, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+    matchesMask = mask.ravel().tolist()
 
-# matchscores will have an entry for each feature in im1.
-# The entry will be 0 if there is not a match.
-# If there is a match, the entry will be the index of the matching feature in im2.
-# matchscores[im1_feat_index] = im2_feat_index, if matchscores[im1_feat_index] != 0
+    # print(H)
+    # print(mask)
 
-# load images and plot
-im1 = array(Image.open(imname1))
-im2 = array(Image.open(imname2))
+    return H, matchesMask
 
-get_matches(l1, l2, matchscores)
+def apply_homography_to_pts(H, pts):
+    src_pts = np.array(pts)
+    # Not sure why, but we need to add an extra dimension to call perspectiveTransform.
+    src_pts = src_pts.reshape(-1, 1, 2)
+    transformed_pts = cv2.perspectiveTransform(src_pts, H)
+    transformed_pts = transformed_pts.reshape(-1, 2)
+    return transformed_pts
 
-plot_matches(im1,im2,l1,l2,matchscores,show_below=True)
-show()
+def main():
+
+    # imname1 = 'C:/Users/Paul/programming-computer-vision-git/PCV/data/climbing_1_small.jpg'
+    # imname2 = 'C:/Users/Paul/programming-computer-vision-git/PCV/data/climbing_2_small.jpg'
+    # imname1 = 'C:/Users/Paul/programming-computer-vision-git/PCV/data/crans_1_small.jpg'
+    # imname2 = 'C:/Users/Paul/programming-computer-vision-git/PCV/data/crans_2_small.jpg'
+    # imname1 = 'C:/Users/Paul/programming-computer-vision-git/PCV/data/house1_small.jpg'
+    # imname2 = 'C:/Users/Paul/programming-computer-vision-git/PCV/data/house2_small.jpg'
+    # imname1 = 'C:/Users/Paul/programming-computer-vision-git/PCV/data/house1_small.jpg'
+    # imname2 = 'C:/Users/Paul/programming-computer-vision-git/PCV/data/house1_small_corner.jpg'
+    imname1 = '/home/paul/computer_vision/images/house1_small.jpg'
+    imname2 = '/home/paul/computer_vision/images/house1_small_corner.jpg'
+
+    # process and save features to file
+    params="--edge-thresh 10 --peak-thresh 5 --verbose"
+    sift.process_image(imname1, imname1+'.sift', params=params)
+    sift.process_image(imname2, imname2+'.sift', params=params)
+
+    # read features and match
+    l1,d1 = sift.read_features_from_file(imname1+'.sift')
+    l2,d2 = sift.read_features_from_file(imname2+'.sift')
+    matchscores = sift.match_twosided(d1, d2)
+
+    # matchscores will have an entry for each feature in im1.
+    # The entry will be 0 if there is not a match.
+    # If there is a match, the entry will be the index of the matching feature in im2.
+    # matchscores[im1_feat_index] = im2_feat_index, if matchscores[im1_feat_index] != 0
+
+    # load images and plot
+    im1 = array(Image.open(imname1))
+    im2 = array(Image.open(imname2))
+
+    pts1, pts2 = get_matches(l1, l2, matchscores)
+
+    H, matches_mask = get_homography(pts1, pts2)
+
+    # Apply the homography, to see if we get similar points.
+    transformed_src_pts = apply_homography_to_pts(H, pts1)
+    print("(Im1 pt) -> (Transformed Im1 pt), [Im2 SIFT pt] - IsInlier")
+    for i in range(len(pts1)):
+        print("(%d, %d) -> (%d, %d), [%d, %d] - %d" % (pts1[i][0], pts1[i][1],
+                                                  transformed_src_pts[i][0], transformed_src_pts[i][1],
+                                                  pts2[i][0], pts2[i][1], matches_mask[i]))
+
+    plot_matches(im1,im2,l1,l2,matchscores,show_below=True)
+    show()
+
+if __name__ == "__main__":
+    main()
