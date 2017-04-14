18a464844a3507ec4733d20366f2c6f46f731881
Paul Morrison <pwmorrison@gmail.com>
2017-04-14 10:17:25 +1000

    Got SIFT VLFeat script working on Linux box. Moved plotting to script, since we modified it slightly.

--- sift_vlfeat.py
+++ sift_vlfeat.py
@@ -1,12 +1,49 @@
 from pylab import *
 from numpy import *
 from PIL import Image
+import os
+print os.environ['HOME']
 
 from PCV.localdescriptors import sift
 
 """
 This is the twosided SIFT feature matching example from Section 2.2 (p 44).
 """
+
+
+def appendimages(im1, im2):
+    """ Return a new image that appends the two images side-by-side. """
+
+    # select the image with the fewest rows and fill in enough empty rows
+    rows1 = im1.shape[0]
+    rows2 = im2.shape[0]
+
+    if rows1 < rows2:
+        im1 = concatenate((im1, zeros((rows2 - rows1, im1.shape[1]))), axis=0)
+    elif rows1 > rows2:
+        im2 = concatenate((im2, zeros((rows1 - rows2, im2.shape[1], im2.shape[2]))), axis=0)
+    # if none of these cases they are equal, no filling needed.
+
+    return concatenate((im1, im2), axis=1)
+
+def plot_matches(im1, im2, locs1, locs2, matchscores, show_below=True):
+    """ Show a figure with lines joining the accepted matches
+        input: im1,im2 (images as arrays), locs1,locs2 (location of features), 
+        matchscores (as output from 'match'), show_below (if images should be shown below). """
+
+    im3 = appendimages(im1, im2)
+    if show_below:
+        im3 = vstack((im3, im3))
+
+    # show image
+    imshow(im3)
+
+    # draw lines for matches
+    cols1 = im1.shape[1]
+    for i, m in enumerate(matchscores):
+        if m > 0:
+            plot([locs1[i][0], locs2[m][0] + cols1], [locs1[i][1], locs2[m][1]], 'c')
+    axis('off')
 
 # imname1 = 'C:/Users/Paul/programming-computer-vision-git/PCV/data/climbing_1_small.jpg'
 # imname2 = 'C:/Users/Paul/programming-computer-vision-git/PCV/data/climbing_2_small.jpg'
@@ -16,8 +53,8 @@ This is the twosided SIFT feature matching example from Section 2.2 (p 44).
 # imname2 = 'C:/Users/Paul/programming-computer-vision-git/PCV/data/house2_small.jpg'
 # imname1 = 'C:/Users/Paul/programming-computer-vision-git/PCV/data/house1_small.jpg'
 # imname2 = 'C:/Users/Paul/programming-computer-vision-git/PCV/data/house1_small_corner.jpg'
-imname1 = 'C:/Users/Paul/programming-computer-vision-git/PCV/data/house1_small.jpg'
-imname2 = 'C:/Users/Paul/programming-computer-vision-git/PCV/data/house1_small_corner.jpg'
+imname1 = '/home/paul/computer_vision/images/house1_small.jpg'
+imname2 = '/home/paul/computer_vision/images/house1_small_corner.jpg'
 
 # process and save features to file
 params="--edge-thresh 10 --peak-thresh 5 --verbose"
@@ -33,5 +70,5 @@ matchscores = sift.match_twosided(d1, d2)
 im1 = array(Image.open(imname1))
 im2 = array(Image.open(imname2))
 
-sift.plot_matches(im1,im2,l1,l2,matchscores,show_below=True)
+plot_matches(im1,im2,l1,l2,matchscores,show_below=True)
 show()
