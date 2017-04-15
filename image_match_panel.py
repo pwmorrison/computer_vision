ad1a37c5f31cce59be573b11efeed84c27ac94db
pwmorrison <pwmorrison@gmail.com>
2017-04-16 07:19:22 +1000

    Load image into a PIL image, so we can do more stuff before displaying the image.

--- image_match_panel.py
+++ image_match_panel.py
@@ -1,14 +1,32 @@
 import wx
+from PIL import Image
 
 """
 Panel for displaying the matches between two images.
 """
 
 class ImageMatchPanel(wx.Panel):
-    def __init__(self, parent, style):
+    def __init__(self, parent, style, image_path_1, image_path_2):
         super(ImageMatchPanel, self).__init__(parent, style=style)
-        # Load the image data into a Bitmap
-        theBitmap = wx.Bitmap("images/house1_small_corner.jpg")
-        # Create a control that can display the
-        # bitmap on the screen.
-        self.bitmap = wx.StaticBitmap(self, bitmap=theBitmap)
+
+        if 0:
+            # Load the image data into a Bitmap
+            theBitmap = wx.Bitmap(image_path_1)
+
+            # Create a control that can display the bitmap on the screen.
+            self.bitmap = wx.StaticBitmap(self, bitmap=theBitmap)
+        else:
+            pilImage = Image.open(image_path_1)
+            self.bitmap = self.static_bitmap_from_pil_image(pilImage)
+
+    def static_bitmap_from_pil_image(caller, pil_image):
+        """
+        From http://stackoverflow.com/questions/39547695/display-pil-image-in-wx-python-in-windows 
+        """
+        wx_image = wx.EmptyImage(pil_image.size[0], pil_image.size[1])
+        wx_image.SetData(pil_image.convert("RGB").tobytes())
+        wx_image.SetAlphaData(pil_image.convert("RGBA").tobytes()[3::4])
+        bitmap = wx.BitmapFromImage(wx_image)
+        static_bitmap = wx.StaticBitmap(caller, wx.ID_ANY, wx.NullBitmap)
+        static_bitmap.SetBitmap(bitmap)
+        return static_bitmap
