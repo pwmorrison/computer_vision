c90eb8fbe013d81a786198a35f08e8db6541e5fa
pwmorrison <pwmorrison@gmail.com>
2017-04-15 18:19:26 +1000

    Made the non-splitter frame the default, because it would be easier to draw lines between images not in a splitter. Also added the toolbar to the default frame.

--- wxpython_app.py
+++ wxpython_app.py
@@ -198,6 +198,69 @@ class MyFrame(wx.Frame):
         # Set the main panel
         self.txt = wx.TextCtrl(self, style=wx.TE_MULTILINE)
 
+        if 1:
+            tb = self.CreateToolBar(TBFLAGS)
+
+            # log.write("Default toolbar tool size: %s\n" % tb.GetToolBitmapSize())
+
+            self.CreateStatusBar()
+
+            tsize = (24, 24)
+            new_bmp = wx.ArtProvider.GetBitmap(wx.ART_NEW, wx.ART_TOOLBAR, tsize)
+            open_bmp = wx.ArtProvider.GetBitmap(wx.ART_FILE_OPEN, wx.ART_TOOLBAR, tsize)
+            copy_bmp = wx.ArtProvider.GetBitmap(wx.ART_COPY, wx.ART_TOOLBAR, tsize)
+            paste_bmp = wx.ArtProvider.GetBitmap(wx.ART_PASTE, wx.ART_TOOLBAR, tsize)
+
+            tb.SetToolBitmapSize(tsize)
+
+            # tb.AddSimpleTool(10, new_bmp, "New", "Long help for 'New'")
+            tb.AddLabelTool(10, "New", new_bmp, shortHelp="New", longHelp="Long help for 'New'")
+            self.Bind(wx.EVT_TOOL, self.OnToolClick, id=10)
+            self.Bind(wx.EVT_TOOL_RCLICKED, self.OnToolRClick, id=10)
+
+            # tb.AddSimpleTool(20, open_bmp, "Open", "Long help for 'Open'")
+            tb.AddLabelTool(20, "Open", open_bmp, shortHelp="Open", longHelp="Long help for 'Open'")
+            self.Bind(wx.EVT_TOOL, self.OnToolClick, id=20)
+            self.Bind(wx.EVT_TOOL_RCLICKED, self.OnToolRClick, id=20)
+
+            tb.AddSeparator()
+            tb.AddSimpleTool(30, copy_bmp, "Copy", "Long help for 'Copy'")
+            self.Bind(wx.EVT_TOOL, self.OnToolClick, id=30)
+            self.Bind(wx.EVT_TOOL_RCLICKED, self.OnToolRClick, id=30)
+
+            tb.AddSimpleTool(40, paste_bmp, "Paste", "Long help for 'Paste'")
+            self.Bind(wx.EVT_TOOL, self.OnToolClick, id=40)
+            self.Bind(wx.EVT_TOOL_RCLICKED, self.OnToolRClick, id=40)
+
+            tb.AddSeparator()
+
+            # tool = tb.AddCheckTool(50, images.Tog1.GetBitmap(), shortHelp="Toggle this")
+            tool = tb.AddCheckLabelTool(50, "Checkable", images.Tog1.GetBitmap(),
+                                        shortHelp="Toggle this")
+            self.Bind(wx.EVT_TOOL, self.OnToolClick, id=50)
+
+            self.Bind(wx.EVT_TOOL_ENTER, self.OnToolEnter)
+            self.Bind(wx.EVT_TOOL_RCLICKED, self.OnToolRClick)  # Match all
+            self.Bind(wx.EVT_TIMER, self.OnClearSB)
+
+            tb.AddSeparator()
+            cbID = wx.NewId()
+
+            tb.AddControl(
+                wx.ComboBox(
+                    tb, cbID, "", choices=["", "This", "is a", "wx.ComboBox"],
+                    size=(150, -1), style=wx.CB_DROPDOWN
+                ))
+            self.Bind(wx.EVT_COMBOBOX, self.OnCombo, id=cbID)
+
+            tb.AddStretchableSpace()
+            # search = TestSearchCtrl(tb, size=(150, -1), doSearch=self.DoSearch)
+            # tb.AddControl(search)
+
+            # Final thing to do for a toolbar is call the Realize() method. This
+            # causes it to render (more or less, that is).
+            tb.Realize()
+
     def DoSetupMenus(self, menubar):
         fileMenu = EasyMenu()
 
@@ -240,9 +303,47 @@ class MyFrame(wx.Frame):
         else:
             event.Skip()
 
+    def OnToolClick(self, event):
+        # self.log.WriteText("tool %s clicked\n" % event.GetId())
+        # tb = self.GetToolBar()
+        tb = event.GetEventObject()
+        tb.EnableTool(10, not tb.GetToolEnabled(10))
+
+    def OnToolRClick(self, event):
+        # self.log.WriteText("tool %s right-clicked\n" % event.GetId())
+        pass
+
+    def OnCombo(self, event):
+        # self.log.WriteText("combobox item selected: %s\n" % event.GetString())
+        pass
+
+    def OnToolEnter(self, event):
+        # self.log.WriteText('OnToolEnter: %s, %s\n' % (event.GetId(), event.GetInt()))
+
+        # if self.timer is None:
+        #     self.timer = wx.Timer(self)
+        #
+        # if self.timer.IsRunning():
+        #     self.timer.Stop()
+
+        # self.timer.Start(2000)
+        event.Skip()
+
+    def OnClearSB(self, event):  # called for the timer event handler
+        self.SetStatusText("")
+        # self.timer.Stop()
+        # self.timer = None
+
+    def OnCloseWindow(self, event):
+        # if self.timer is not None:
+        #     self.timer.Stop()
+        #     self.timer = None
+        self.Destroy()
+
+
 class MyApp(wx.App):
     def OnInit(self):
-        if 0:
+        if 1:
             self.frame = MyFrame(None, title="Main Frame")
         else:
             self.frame = SplitterFrame(None, title="Splitter frame")
