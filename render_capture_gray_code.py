307fa423ee1d623a9481c580b80c459447fba3c2
pwmorrison <pwmorrison@gmail.com>
2017-08-14 18:03:15 +1000

    Several changes to get horizontal and vertical gray code projection and capture working.

--- render_capture_gray_code.py
+++ render_capture_gray_code.py
@@ -1,6 +1,4 @@
 import wx
-import time
-import numpy as np
 import cv2
 from PIL import Image
 from gray_code import generate_gray_code_sequence, generate_gray_code_bit_planes
@@ -8,33 +6,43 @@ from gray_code_camera_panel import GrayCodeCameraPanel
 from image_render_panel import ImageRenderPanel
 import pygame
 import pygame.camera
+import sys
 
 """
-Main file for rendering gray code patterns, and capturing images of the patterns.
+Main file for rendering gray code patterns, and capturing images of the 
+patterns.
 """
 
-capture_library = "streamer"
+capture_library = "opencv"
+
 
 class GrayCodeController():
-    def __init__(self, full_screen, pos_neg_codes, capture_images, display_captured_images):
+    def __init__(self, full_screen, pos_neg_codes, capture_images,
+                 display_captured_images, horizontal, vertical):
         app = wx.App(False)
+        self.app = app
         # Create frame, no parent, -1 is default ID.
         if not full_screen:
-            window_size = (500, 500)
-            frame = wx.Frame(None, -1, "Gray code projection", size=window_size)
+            self.window_size = (500, 500)
+            frame = wx.Frame(
+                None, -1, "Gray code projection", size=self.window_size)
         else:
-            window_size = wx.GetDisplaySize()
-            frame = wx.Frame(None, -1, "Gray code projection", size=window_size, style=wx.NO_BORDER)
+            self.window_size = wx.GetDisplaySize()
+            frame = wx.Frame(
+                None, -1, "Gray code projection", size=self.window_size,
+                style=wx.NO_BORDER)
             frame.Maximize(True)
         # Add a panel to the frame, -1 is default ID
-        self.gray_code_panel = GrayCodePanel(frame, -1, window_size)
+        self.gray_code_panel = GrayCodePanel(frame, -1, self.window_size)
         # Show the frame
         frame.Show(True)
+
+        self.frame = frame
 
         if capture_images:
             # Set up the camera.
             if capture_library == "opencv":
-                camera_number = 0
+                camera_number = 2
                 capture = cv2.VideoCapture(camera_number)
             elif capture_library == "pygame":
                 pygame.init()
@@ -46,8 +54,10 @@ class GrayCodeController():
             elif capture_library == "streamer":
                 capture = None
 
-            camera_frame = wx.Frame(None, -1, "Camera capture", size=window_size)
-            self.camera_panel = GrayCodeCameraPanel(camera_frame, capture, capture_library)
+            camera_frame = wx.Frame(
+                None, -1, "Camera capture", size=self.window_size)
+            self.camera_panel = GrayCodeCameraPanel(
+                camera_frame, capture, capture_library)
 
             if display_captured_images:
                 camera_frame.Show(True)
@@ -55,22 +65,32 @@ class GrayCodeController():
         # Timer to kick off processing, after the go into the event loop.
         self.timer = wx.Timer()
         app.Bind(wx.EVT_TIMER, self.processing_loop, self.timer)
-        self.timer.Start(1000)
+        self.timer.Start(500)
 
-        self.gray_code_state = GrayCodeState(window_size)
+        self.gray_code_state = GrayCodeState(self.window_size)
 
         self.iteration = 0
         self.black_iteration = 0
         self.white_iteration = 1
         self.first_gray_code_iteration = 2
-        self.last_gray_code_iteration = self.first_gray_code_iteration + self.gray_code_state.get_num_bit_planes()
+        self.last_gray_code_iteration = self.first_gray_code_iteration \
+            + self.gray_code_state.get_num_bit_planes()
 
-        # Whether to render and capture both positive and negative gray codes, for robustness.
-        # A positive frame has 0 represented as black, and 1 as white. A negative frame is the opposite.
+        self.horizontal = horizontal
+        self.vertical = vertical
+        self.horizontal_iterations = self.horizontal
+        assert(self.horizontal or self.vertical)
+
+        # Whether to render and capture both positive and negative gray codes, 
+        # for robustness.
+        # A positive frame has 0 represented as black, and 1 as white. A 
+        # negative frame is the opposite.
         self.pos_neg_codes = pos_neg_codes
         self.is_pos_frame = True
 
         self.is_render = True
+
+        self.finished = False
 
         # Start the event loop
         app.MainLoop()
@@ -79,6 +99,9 @@ class GrayCodeController():
         """
         Processes a timer event, to cause Gray code rendering and capture.
         """
+        if self.finished:
+            return
+
         if self.iteration == self.black_iteration:
             if self.is_render:
                 # Render a black frame.
@@ -109,7 +132,7 @@ class GrayCodeController():
                 self.gray_code_panel.Refresh()
                 self.is_render = False
             else:
-                # Capture the black frame.
+                # Capture the white frame.
                 image = self.camera_panel.capture_image()
                 image = image.convert('L')
 
@@ -117,7 +140,7 @@ class GrayCodeController():
                 self.camera_panel.Refresh()
 
                 # image = Image.fromarray(gray_image)
-                print("Captured image:", image)
+                print("Captured white image:", image)
                 # image = image.convert('gray')
                 image.save("white.png")
 
@@ -125,10 +148,14 @@ class GrayCodeController():
                 self.iteration += 1
         else:
             if self.is_render:
-                print("Rendering")
+                print("Rendering gray code pattern %d of %d." %
+                      (self.iteration - self.first_gray_code_iteration,
+                       self.last_gray_code_iteration - self.first_gray_code_iteration))
                 self.gray_code_panel.render_black = False
                 self.gray_code_panel.render_white = False
                 # Rendering a Gray code pattern.
+
+                self.gray_code_panel.horizontal = self.horizontal_iterations
 
                 if self.iteration != self.first_gray_code_iteration:
                     if self.pos_neg_codes:
@@ -148,7 +175,9 @@ class GrayCodeController():
 
                 self.is_render = False
             else:
-                print("Capturing")
+                print("Capturing gray code pattern %d of %d." %
+                      (self.iteration - self.first_gray_code_iteration,
+                       self.last_gray_code_iteration - self.first_gray_code_iteration))
                 if self.iteration < self.last_gray_code_iteration:
                     # Capturing an Gray code pattern.
                     image = self.camera_panel.capture_image()
@@ -161,11 +190,38 @@ class GrayCodeController():
                     print("Captured image:", image)
                     # image = image.convert('gray')
 
+                    if self.horizontal_iterations:
+                        direction_label = "horiz"
+                    else:
+                        direction_label = "vert"
+
                     if self.pos_neg_codes:
                         pos_neg_label = "pos" if self.is_pos_frame else "neg"
-                        image.save("graycode_%02d_%s.png" % (self.iteration - self.first_gray_code_iteration, pos_neg_label))
+                        image.save("graycode_%02d_%s_%s.png" %
+                                   (self.iteration - self.first_gray_code_iteration,
+                                    direction_label, pos_neg_label))
                     else:
-                        image.save("graycode_%02d.png" % (self.iteration - self.first_gray_code_iteration))
+                        image.save("graycode_%02d_%s.png" %
+                                   (self.iteration - self.first_gray_code_iteration,
+                                    direction_label))
+                else:
+                    # We've reached the end of the sequence.
+                    print("Reached end of sequence.")
+                    if self.horizontal_iterations and self.vertical:
+                        # Proceed to vertical gray code.
+                        self.horizontal_iterations = False
+                        self.iteration = self.first_gray_code_iteration
+                        self.is_render = True
+                        self.gray_code_state = GrayCodeState(self.window_size)
+                        if self.pos_neg_codes:
+                            self.is_pos_frame = not self.is_pos_frame
+                        return
+                    else:
+                        # End the script.
+                        # self.frame.Close()
+                        # self.app.Destroy()
+                        # sys.exit(0)
+                        self.finished = True
 
                 self.is_render = True
                 # self.iteration += 1
@@ -237,6 +293,7 @@ class GrayCodePanel(wx.Panel):
         self.render_black = False
         self.render_white = False
         self.is_pos_frame = True
+        self.horizontal = True
 
         self.Bind(wx.EVT_PAINT, self.OnPaint)
 
@@ -252,10 +309,11 @@ class GrayCodePanel(wx.Panel):
 
     def OnPaint(self, event):
         # print("OnPaint")
-        dc = wx.PaintDC(self)
 
         # Render static stuff, that persists between re-paints.
         if 0:
+            dc = wx.PaintDC(self)
+
             #blue non-filled rectangle
             dc.SetPen(wx.Pen("blue"))
             dc.SetBrush(wx.Brush("blue", wx.TRANSPARENT)) #set brush transparent for non-filled rectangle
@@ -273,10 +331,13 @@ class GrayCodePanel(wx.Panel):
                 dc.DrawRectangle(220 + i*10,10,200 + i*10,200)
 
         if self.render_black:
+            print("Rendering black")
             self.SetBackgroundColour("black")
         elif self.render_white:
+            print("Rendering white")
             self.SetBackgroundColour("white")
         elif self.gray_code_state is not None:
+            print("Rendering gray code")
             self.render_gray_code(self.gray_code_state, self.is_pos_frame)
 
     def timer_update(self, event):
@@ -307,8 +368,10 @@ class GrayCodePanel(wx.Panel):
         # state = event.gray_code_state
 
         if is_pos_frame:
+            print("Pos background.")
             self.SetBackgroundColour("black")
         else:
+            print("Neg background.")
             self.SetBackgroundColour("white")
 
         # Get the current bit plane.
@@ -320,23 +383,32 @@ class GrayCodePanel(wx.Panel):
 
         if 0:
             dc.SetPen(wx.Pen("blue"))
-            dc.SetBrush(wx.Brush("blue", wx.TRANSPARENT)) #set brush transparent for non-filled rectangle
+            # set brush transparent for non-filled rectangle
+            dc.SetBrush(wx.Brush("blue", wx.TRANSPARENT)) 
             dc.DrawRectangle(10 + bit_plane * 20, 310, 200, 200)
 
         if is_pos_frame:
+            print("Pos foreground.")
             dc.SetPen(wx.Pen("white"))
             dc.SetBrush(wx.Brush("white"))
         else:
+            print("Neg foreground.")
             dc.SetPen(wx.Pen("black"))
             dc.SetBrush(wx.Brush("black"))
         # dc.DrawRectangle(10 + bit_plane * 20, 310, 200, 200)
         for x, bit_val in enumerate(bit_plane):
             if bit_val == 1:
-                dc.DrawRectangle(x, 0, 1, self.window_size[1])
+                if self.horizontal:
+                    dc.DrawRectangle(x, 0, 1, self.window_size[1])
+                else:
+                    dc.DrawRectangle(0, x, self.window_size[0], 1)
+
 
 if __name__ == '__main__':
-    full_screen = True
-    pos_neg_codes = True
+    full_screen = False
+    pos_neg_codes = False
     capture_images = True
     display_captured_images = False
-    controller = GrayCodeController(full_screen, pos_neg_codes, capture_images, display_captured_images)
+    controller = GrayCodeController(
+        full_screen, pos_neg_codes, capture_images, display_captured_images,
+        horizontal=True, vertical=True)
