4da64af34af92bedb8b62ad1a9076fa5b90da2bb
pwmorrison <pwmorrison@gmail.com>
2017-08-18 17:53:00 +1000

    Started making a Pygame app, with the possible intention of it replacing wxPython.

---
+++ pygame_app.py
@@ -0,0 +1,24 @@
+import pygame
+
+def main():
+    pygame.init()
+
+    gameDisplay = pygame.display.set_mode((800,600))
+    pygame.display.set_caption('A bit Racey')
+
+    clock = pygame.time.Clock()
+
+    crashed = False
+    while not crashed:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                crashed = True
+
+            print(event)
+
+        pygame.display.update()
+        clock.tick(60)
+
+if __name__ == '__main__':
+    main()
