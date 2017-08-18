4576320a9c371312e66211c1c5046c48bfd0c4f4
pwmorrison <pwmorrison@gmail.com>
2017-08-18 19:42:30 +1000

    Added gray code timer.

--- pygame_app.py
+++ pygame_app.py
@@ -1,4 +1,5 @@
 import pygame
+from gray_code import generate_gray_code_sequence, generate_gray_code_bit_planes
 
 def process_gray_code():
     print("Processing gray code.")
@@ -6,6 +7,51 @@ def process_gray_code():
 
 def car(x, y, gameDisplay, carImg):
     gameDisplay.blit(carImg, (x,y))
+
+class GrayCodeState():
+    """
+    Class representing the sequence of gray code projections.
+    This class supplies the bit planes that are rendered, and keeps track of the sequence of rendered bit planes.
+    """
+    def __init__(self, window_size):
+        width = window_size[0]
+        height = window_size[1]
+        # Generate the gray code sequences to cover the largest possible coordinate.
+        gray_code_arrays = generate_gray_code_sequence(max(width, height))
+
+        # Convert the sequence to bit planes.
+        self.bit_planes = generate_gray_code_bit_planes(gray_code_arrays)
+
+        self.current_bit_plane = 0
+        self.final_bit_plane = self.bit_planes.shape[0] - 1
+        self.sequence_finished = False
+
+    def progress_state(self):
+        """
+        Progress the current state to the next state.
+        """
+        self.current_bit_plane += 1
+        if self.current_bit_plane > self.final_bit_plane:
+            self.current_bit_plane = self.final_bit_plane
+            self.sequence_finished = True
+
+    def is_sequence_finished(self):
+        return self.sequence_finished
+
+    def get_current_bit_plane(self):
+        """
+        Gets the bit plane for the current state.
+        """
+        # TODO For now, just return the current bit plane number.
+        bit_plane_index = len(self.bit_planes) - self.current_bit_plane - 1
+        #print("Getting bit plane for index:", bit_plane_index)
+        return self.bit_planes[bit_plane_index]
+        # return self.bit_planes[self.current_bit_plane]
+
+    def get_num_bit_planes(self):
+        return len(self.bit_planes)
+
+GRAYCODEEVENT = pygame.USEREVENT + 1
 
 def main():
     pygame.init()
@@ -27,6 +73,8 @@ def main():
     x_change = 0
     car_speed = 0
 
+    gray_code_state = GrayCodeState((display_width, display_height))
+
     crashed = False
     gray_code = False
     while not crashed:
@@ -42,11 +90,20 @@ def main():
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     x_change = 0
+                if event.key == pygame.K_g:
+                    if gray_code == False:
+                        print("Starting gray code.")
+                        pygame.time.set_timer(GRAYCODEEVENT, 1000)
+                        gray_code = True
+                    else:
+                        print("Stopping gray code.")
+                        pygame.time.set_timer(GRAYCODEEVENT, 0)
+                        gray_code = False
+            if event.type == GRAYCODEEVENT:
+                print("Rendering / capturing gray code frame.")
+                process_gray_code()
             print(event)
         x += x_change
-
-        if gray_code:
-            process_gray_code()
 
         gameDisplay.fill(white)
         car(x, y, gameDisplay, carImg)
